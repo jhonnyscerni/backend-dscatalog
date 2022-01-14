ac31158547a03b914f23a0a7ed7db0751d472e20
jhonnyscerni <jhonnyscerni@gmail.com>
2022-01-13 21:42:10 -0300

    Refatoração no Pageable

--- src/main/java/br/com/siberius/dscatalog/resources/ProductResource.java
+++ src/main/java/br/com/siberius/dscatalog/resources/ProductResource.java
@@ -2,10 +2,8 @@ package br.com.siberius.dscatalog.resources;
 
 import br.com.siberius.dscatalog.dto.ProductDTO;
 import br.com.siberius.dscatalog.services.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -16,21 +14,15 @@ import java.net.URI;
 @RequestMapping(value = "/products")
 public class ProductResource {
 
-    @Autowired
-    private ProductService service;
+    private final ProductService service;
+
+    public ProductResource(ProductService service) {
+        this.service = service;
+    }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-    ) {
-
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-
-        Page<ProductDTO> list = service.findAllPaged(pageRequest);
-
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> list = service.findAllPaged(pageable);
         return ResponseEntity.ok().body(list);
     }
 
